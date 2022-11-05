821a3e5f392dd6c131edc37166ef9f01914f50d8
Lidiya <lidia.pahno@yandex.ru>
2022-11-06 02:47:34 +0300

    add task 29

---
+++ task29/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив c клавиатуры
+// из 8 элементов и выводит их на экран.
+
+int[] New()
+{
+    Console.Write("Введите значения массива через запятую c пробелом: ");
+    string[] sign = Console.ReadLine()!.Split(", ");
+    int[] array;
+    if (sign.Length > 8)
+    {
+        array = new int[sign.Length];
+    }
+    else
+    {
+        array = new int[8];
+    }
+    for (int i = 0; i < sign.Length; i++)
+    {
+        array[i] = System.Int32.Parse(sign[i]);
+
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Вывод заданного массива: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i != arr.Length - 1)
+        {
+            Console.Write(", ");
+
+        }
+    }
+
+}
+
+PrintArray(New());
